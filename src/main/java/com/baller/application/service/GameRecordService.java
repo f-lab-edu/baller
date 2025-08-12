1e860a23008a5b9a288df3347d33f2964b929e69
baltol96 <baltol@naver.com>
2025-08-12 23:51:06 +0900

    - SSE broadcast 비동기로 변경

--- src/main/java/com/baller/application/service/GameRecordService.java
+++ src/main/java/com/baller/application/service/GameRecordService.java
@@ -7,6 +7,7 @@ import com.baller.domain.enums.SseEventType;
 import com.baller.presentation.dto.request.game.GameRecordResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,23 +18,20 @@ public class GameRecordService {
     private final SseEmitterManager sseEmitterManager;
     private final GameService gameService;
 
+    public GameRecordResponse getCurrentRecord(Long gameId) {
+        return gameService.getGame(gameId);
+    }
+
     public SseEmitter subscribe(Long gameId) {
 
         return sseEmitterManager.subscribe(SseChannelKeyType.GAME_RECORD.of(gameId));
 
     }
 
-    public void updateLiveRecord(Long gameId, Object data) {
-        sseEmitterManager.broadcast(SseChannelKeyType.GAME_RECORD.of(gameId), data, SseEventType.GAME_UPDATE);
-    }
-
+    @Async("sseListenerPool")
     @EventListener
     public void gameRecordUpdate(GameUpdatedEvent gameUpdatedEvent) {
         sseEmitterManager.broadcast(SseChannelKeyType.GAME_RECORD.of(gameUpdatedEvent.getGameId()), gameUpdatedEvent.getData(), SseEventType.GAME_UPDATE);
-    }
-
-    public GameRecordResponse getCurrentRecord(Long gameId) {
-        return gameService.getGame(gameId);
     }
 
 }
